c7b62c9e7ce870637775d362fa89c5f98010e941
yshahak <yshahak@gmail.com>
2015-08-12 21:02:06 +0300

    add

--- app/src/main/java/com/ysapps/tools/videoslocker/activities/MainActivity.java
+++ app/src/main/java/com/ysapps/tools/videoslocker/activities/MainActivity.java
@@ -1,12 +1,10 @@
 package com.ysapps.tools.videoslocker.activities;
 
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 
 import com.ysapps.tools.videoslocker.R;
-import com.ysapps.tools.videoslocker.system.Utils;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,9 +12,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Cursor videoCursor = Utils.getAllVideos(this);
-        int columnCount = videoCursor.getColumnCount();
+        setContentView(R.layout.main);
+        /*Cursor videoCursor = Utils.getAllVideos(this);
+        int columnCount = videoCursor.getColumnCount();*/
     }
 
     @Override
--- app/src/main/java/com/ysapps/tools/videoslocker/fragments/FragmentVideos.java
+++ app/src/main/java/com/ysapps/tools/videoslocker/fragments/FragmentVideos.java
@@ -1,27 +1,62 @@
 package com.ysapps.tools.videoslocker.fragments;
 
+import android.app.Activity;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.ysapps.tools.videoslocker.R;
+import com.ysapps.tools.videoslocker.system.ContractListFragment;
 
 /**
  * Created by yshahak on 12/08/2015.
  */
-public class FragmentVideos extends Fragment implements LoaderManager.LoaderCallbacks {
+public class FragmentVideos extends ContractListFragment<VideosFragment.Contract>
+            implements LoaderManager.LoaderCallbacks<Cursor>, SimpleCursorAdapter.ViewBinder {
 
     private final int ID_LOADER_VIDEO = 0;
+    ImageLoader imageLoader;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        String[] from = { MediaStore.Video.Media.TITLE, MediaStore.Video.Media._ID };
+        int[] to= { android.R.id.text1, R.id.thumbnail };
+        SimpleCursorAdapter adapter=
+                    new SimpleCursorAdapter(
+                                getActivity(),
+                                R.layout.row, // the layout inflate
+                                null,         //cursor
+                                from,         //the columns we want from the content provider
+                                to,           //this ids inside the layout
+                                0); //flags
+        adapter.setViewBinder(this); //for bind the thumbnail and the results from cursor
+        setListAdapter(adapter);
         getLoaderManager().initLoader(ID_LOADER_VIDEO, null, this);
     }
 
     @Override
-    public Loader onCreateLoader(int id, Bundle args) {
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ImageLoaderConfiguration ilConfig = new ImageLoaderConfiguration.Builder(activity).build();
+        imageLoader = ImageLoader.getInstance();
+        imageLoader.init(ilConfig);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         if (id == ID_LOADER_VIDEO){
             return(new CursorLoader(
                         getActivity(),
@@ -35,12 +70,26 @@ public class FragmentVideos extends Fragment implements LoaderManager.LoaderCall
     }
 
     @Override
-    public void onLoadFinished(Loader loader, Object data) {
-
+    public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+        ((CursorAdapter)getListAdapter()).swapCursor(c);
+    }
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        ((CursorAdapter)getListAdapter()).swapCursor(null);
     }
 
     @Override
-    public void onLoaderReset(Loader loader) {
-
+    public boolean setViewValue(View v, Cursor c, int column) {
+        if (column == c.getColumnIndex(MediaStore.Video.Media._ID)) {
+            Uri video = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, c.getInt(column));
+            DisplayImageOptions opts = new DisplayImageOptions.Builder()
+                        .showImageOnLoading(android.R.drawable.presence_video_online)
+                        .build();
+            imageLoader.displayImage(video.toString(), (ImageView)v, opts);
+            return(true);
+        }
+        return false;
     }
+
+
 }
